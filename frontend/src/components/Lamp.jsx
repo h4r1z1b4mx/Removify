bb6f79b20f4ab5a0edc637201cdd79ef22b22b52
h4r1z1b4mx <h4r1z1b4mx@gmail.com>
2025-04-10 23:31:23 +0530

    updated the Lamp ui

--- frontend/src/components/Lamp.jsx
+++ frontend/src/components/Lamp.jsx
@@ -16,7 +16,7 @@ export function LampDemo() {
           ease: "easeInOut",
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Build lamps <br /> the right way
+        We Make <br /> Backgrounds Ghost.
       </motion.h1>
     </LampContainer>
   );
